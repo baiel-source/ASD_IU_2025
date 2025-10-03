360a851dd94c5a3eba50e148fc01adbfcc722ab5
hiyokin <daniilp357@gmail.com>
2025-10-03 20:00:22 +0300

    fix

--- lab/lab1/BinarySearch.java
+++ lab/lab1/BinarySearch.java
@@ -1,4 +1,121 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class BinarySearch {
+//Реализовать алгоритм бинарного поиска двумя способами.
+class BinarySearch {
+
+    public static void main() {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите длину массива : ");
+        int arrayLen = in.nextInt();
+        int[] arr = new int[arrayLen];
+        for (int i = 0; i < arrayLen; i++) {
+            System.out.print("Введите элемент: ");
+            arr[i] = in.nextInt();
+        }
+
+        sort(arr);
+
+        System.out.print("Введите элемент, который хотите найти в массиве: ");
+        int x = in.nextInt();
+
+        binarySearch1(arr, x);
+
+        int index = binarySearch2(arr, x, 0, arrayLen - 1);
+        output(index);
+
+        in.close();
+    }
+
+    static void sort(int[] arr) {
+        int flag = 0;
+
+        boolean isNotSorted = check(flag, arr);
+
+        if (isNotSorted) {
+            bubbleSort(arr);
+        }
+    }
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    static boolean check(int flag, int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] >= arr[i + 1]) {
+                flag++;
+            }
+            if (arr[i] <= arr[i + 1]) {
+                flag--;
+            }
+        }
+        return flag != n - 1 && flag != 1 - n;
+    }
+
+    static void binarySearch1(int[] arr, int x) {
+        int n = arr.length;
+        int left = 0;
+        int right = n - 1;
+        int found = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == x) {
+                found = 1;
+                break;
+            }
+            if (arr[mid] < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (found == 1) {
+            System.out.println("Элемент есть");
+        } else {
+            System.out.println("Элемента нет");
+        }
+    }
+
+    static int binarySearch2(int[] arr, int x, int left, int right) {
+
+        if (left > right) {
+            return -1;
+        }
+
+        int middle = left + (right - left) / 2;
+
+        if (arr[middle] < x) {
+            return binarySearch2(arr, x, middle + 1, right);
+        }
+
+        if (arr[middle] > x) {
+            return binarySearch2(arr, x, left, middle - 1);
+        }
+
+        return middle;
+    }
+
+    static void output(int index) {
+        if (index != -1) {
+            System.out.println("Элемент есть");
+        } else {
+            System.out.println("Элемента нет");
+        }
+    }
+
 }
--- lab/lab1/DivisionBinaryArray.java
+++ lab/lab1/DivisionBinaryArray.java
@@ -1,4 +1,75 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class DivisionBinaryArray {
+/*Дан массив целых чисел, представляющий двоичное число.
+Пример, дан массив bi_arr = [1, 1, 0]. Этот массив в десятичной системе
+выглядит так: arr = [1, 3, 6]. То есть:
+• arr[0] = bi_arr[0] = 1! = 1"#,
+• arr[1] = bi_arr[0] bi_arr[1] =11! = 3"#,
+• arr[2] = bi_arr[0] bi_arr[1] bi_arr[2] =110! = 6"#
+Так же дано целое положительное число – n. Вернуть массив Boolean, где
+true – число делится на N, false – нет.
+Пусть n = 6, тогда для предыдущего примера результат должен выглядеть
+так: [false, false, true].
+Примечание. Делитель тоже необходимо ввести с клавиатуры.*/
+
+class DivisionBinaryArray {
+
+    public static void main() {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите длину массива : ");
+        int arrayLen = in.nextInt();
+        int[] arr = new int[arrayLen];
+        for (int i = 0; i < arrayLen; i++) {
+            System.out.print("Введите элемент: ");
+            arr[i] = in.nextInt() % 2;
+        }
+
+        System.out.print("Введите делитель: ");
+        int divisor = in.nextInt();
+
+        int[] newArr = conventing(arr);
+
+        boolean[] boolArr = division(newArr, divisor);
+
+        output(boolArr);
+
+        in.close();
+    }
+
+    static int[] conventing(int[] arr) {
+        int n = arr.length;
+        int[] newArr = new int[n];
+
+        newArr[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            newArr[i] = arr[i] + newArr[i - 1] * 2;
+        }
+        return newArr;
+    }
+
+    static boolean[] division(int[] newArr, int divisor) {
+        int n = newArr.length;
+        boolean[] boolArr = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            boolArr[i] = false;
+            if (newArr[i] % divisor == 0) {
+                boolArr[i] = true;
+            }
+        }
+        return boolArr;
+    }
+
+    static void output(boolean[] arr) {
+        int n = arr.length;
+        System.out.print("[");
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) {
+                System.out.print(arr[i] + "]");
+                break;
+            }
+            System.out.print(arr[i] + ", ");
+        }
+    }
 }
