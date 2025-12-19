ecec3ab069351e2baa09d10a960c36b5cfe5c82a
baiel-source <baiel07@yandex.ru>
2025-12-19 23:46:29 +0300

    Update ArrayMultiplication.java

--- lab/lab1/src/ArrayMultiplication.java
+++ lab/lab1/src/ArrayMultiplication.java
@@ -3,53 +3,84 @@ import java.util.Scanner;
 
 public class ArrayMultiplication {
 
-    public static int[] multiplyNumbers(int[] num1, int[] num2) {
-        if (num1 == null || num2 == null) {
+    private static final String CONTINUE_COMMAND = "да";
+    private static final String PROGRAM_TITLE = "=== Умножение чисел-массивов ===";
+    private static final int MIN_DIGIT_VALUE = 0;
+    private static final int MAX_DIGIT_VALUE = 9;
+
+    public static int[] multiplyNumbers(int[] firstNumber, int[] secondNumber) {
+        validateInputArrays(firstNumber, secondNumber);
+        
+        if (isZero(firstNumber) || isZero(secondNumber)) {
+            return new int[]{0};
+        }
+        
+        int[] multiplicationResult = performMultiplication(firstNumber, secondNumber);
+        return removeLeadingZeros(multiplicationResult);
+    }
+    
+    private static void validateInputArrays(int[] firstArray, int[] secondArray) {
+        if (firstArray == null || secondArray == null) {
             throw new IllegalArgumentException("Массивы не могут быть null");
         }
-
-        for (int digit : num1) {
-            if (digit < 0 || digit > 9) throw new IllegalArgumentException("Некорректная цифра");
-        }
-        for (int digit : num2) {
-            if (digit < 0 || digit > 9) throw new IllegalArgumentException("Некорректная цифра");
-        }
-
-        if ((num1.length == 1 && num1[0] == 0) || (num2.length == 1 && num2[0] == 0)) {
-            return new int[]{0};
-        }
-
-        int len1 = num1.length;
-        int len2 = num2.length;
-        int[] result = new int[len1 + len2];
-
-        for (int i = len1 - 1; i >= 0; i--) {
-            for (int j = len2 - 1; j >= 0; j--) {
-                int product = num1[i] * num2[j];
-                int sum = product + result[i + j + 1];
-                result[i + j + 1] = sum % 10;
-                result[i + j] += sum / 10;
+        
+        validateDigits(firstArray);
+        validateDigits(secondArray);
+    }
+    
+    private static void validateDigits(int[] numberArray) {
+        for (int digit : numberArray) {
+            if (digit < MIN_DIGIT_VALUE || digit > MAX_DIGIT_VALUE) {
+                throw new IllegalArgumentException("Некорректная цифра");
             }
         }
-
-        int firstNonZero = 0;
-        while (firstNonZero < result.length && result[firstNonZero] == 0) {
-            firstNonZero++;
+    }
+    
+    private static boolean isZero(int[] numberArray) {
+        return numberArray.length == 1 && numberArray[0] == 0;
+    }
+    
+    private static int[] performMultiplication(int[] firstNumber, int[] secondNumber) {
+        int firstNumberLength = firstNumber.length;
+        int secondNumberLength = secondNumber.length;
+        int[] result = new int[firstNumberLength + secondNumberLength];
+        
+        for (int firstIndex = firstNumberLength - 1; firstIndex >= 0; firstIndex--) {
+            for (int secondIndex = secondNumberLength - 1; secondIndex >= 0; secondIndex--) {
+                int product = firstNumber[firstIndex] * secondNumber[secondIndex];
+                int sum = product + result[firstIndex + secondIndex + 1];
+                result[firstIndex + secondIndex + 1] = sum % 10;
+                result[firstIndex + secondIndex] += sum / 10;
+            }
         }
-
-        if (firstNonZero == result.length) {
+        
+        return result;
+    }
+    
+    private static int[] removeLeadingZeros(int[] numberArray) {
+        int firstNonZeroIndex = findFirstNonZeroDigit(numberArray);
+        
+        if (firstNonZeroIndex == numberArray.length) {
             return new int[]{0};
         }
-
-        return Arrays.copyOfRange(result, firstNonZero, result.length);
+        
+        return Arrays.copyOfRange(numberArray, firstNonZeroIndex, numberArray.length);
+    }
+    
+    private static int findFirstNonZeroDigit(int[] numberArray) {
+        int index = 0;
+        while (index < numberArray.length && numberArray[index] == 0) {
+            index++;
+        }
+        return index;
     }
 
     public static String arrayToString(int[] number) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
         for (int digit : number) {
-            sb.append(digit);
+            stringBuilder.append(digit);
         }
-        return sb.toString();
+        return stringBuilder.toString();
     }
 
     public static int[] readNumberArrayFromUser(String numberName) {
@@ -66,16 +97,16 @@ public class ArrayMultiplication {
         int[] number = new int[size];
         System.out.println("Введите " + size + " цифр:");
 
-        for (int i = 0; i < size; i++) {
-            System.out.print("Цифра " + (i + 1) + ": ");
+        for (int index = 0; index < size; index++) {
+            System.out.print("Цифра " + (index + 1) + ": ");
             int digit = scanner.nextInt();
 
-            while (digit < 0 || digit > 9) {
-                System.out.print("Цифра должна быть от 0 до 9: ");
+            while (digit < MIN_DIGIT_VALUE || digit > MAX_DIGIT_VALUE) {
+                System.out.print("Цифра должна быть от " + MIN_DIGIT_VALUE + " до " + MAX_DIGIT_VALUE + ": ");
                 digit = scanner.nextInt();
             }
 
-            number[i] = digit;
+            number[index] = digit;
         }
 
         return number;
@@ -83,30 +114,30 @@ public class ArrayMultiplication {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean continueProgram = true;
+        boolean shouldContinue = true;
 
-        System.out.println("=== Умножение чисел-массивов ===");
+        System.out.println(PROGRAM_TITLE);
 
-        while (continueProgram) {
+        while (shouldContinue) {
             try {
-                int[] number1 = readNumberArrayFromUser("первом числе");
-                int[] number2 = readNumberArrayFromUser("втором числе");
+                int[] firstNumber = readNumberArrayFromUser("первом числе");
+                int[] secondNumber = readNumberArrayFromUser("втором числе");
 
-                System.out.println("Первое число: " + arrayToString(number1));
-                System.out.println("Второе число: " + arrayToString(number2));
+                System.out.println("Первое число: " + arrayToString(firstNumber));
+                System.out.println("Второе число: " + arrayToString(secondNumber));
 
-                int[] result = multiplyNumbers(number1, number2);
+                int[] result = multiplyNumbers(firstNumber, secondNumber);
                 System.out.println("Результат: " + arrayToString(result));
 
-            } catch (IllegalArgumentException e) {
-                System.out.println("Ошибка: " + e.getMessage());
+            } catch (IllegalArgumentException exception) {
+                System.out.println("Ошибка: " + exception.getMessage());
             }
 
             System.out.print("Продолжить? (да/нет): ");
-            String answer = scanner.next();
+            String userAnswer = scanner.next();
 
-            if (!answer.equalsIgnoreCase("да")) {
-                continueProgram = false;
+            if (!userAnswer.equalsIgnoreCase(CONTINUE_COMMAND)) {
+                shouldContinue = false;
             }
         }
 
